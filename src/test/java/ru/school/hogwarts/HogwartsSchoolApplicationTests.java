32d7273c8951ec73ee8416956872f6b3d0d82641
Aleksandr Katyushnyj <a-katushnii@yandex.ru>
2023-09-06 19:39:40 +0300

    Initial commit

---
+++ src/test/java/ru/school/hogwarts/HogwartsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.school.hogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
