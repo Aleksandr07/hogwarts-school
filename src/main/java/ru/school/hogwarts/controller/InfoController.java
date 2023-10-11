73ed175e407810c6ece8b2d16c2931f2873f97a2
Aleksandr <a-katushnii@yandex.ru>
2023-10-12 01:07:45 +0300

    Выполнил задания урока 4.4

---
+++ src/main/java/ru/school/hogwarts/controller/InfoController.java
@@ -0,0 +1,22 @@
+package ru.school.hogwarts.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.school.hogwarts.service.InfoService;
+
+@RestController
+public class InfoController {
+
+    private final InfoService infoService;
+
+    public InfoController(InfoService infoService) {
+        this.infoService = infoService;
+    }
+
+    @GetMapping("/getPort")
+    public ResponseEntity<Integer> getPort() {
+        Integer port = infoService.getPort();
+        return ResponseEntity.ok(port);
+    }
+}
